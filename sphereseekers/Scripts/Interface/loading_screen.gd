d83ada4c18720b1e78b1c291f6cb2c2d99e623a2
KOrtizLedezma <kenethortiz2016@gmail.com>
2025-03-23 03:18:57 -0400

    Styles for interfaces implemented

--- sphereseekers/Scripts/Interface/loading_screen.gd
+++ sphereseekers/Scripts/Interface/loading_screen.gd
@@ -1,72 +1,71 @@
 extends Control
 
-var scene_path: String = "" 
-var loading_complete = false
+# UI References
+var bg_rect: ColorRect
+var gear_image: TextureRect
 
-func _ready():
-	var background = $background
-	var image = $image
-	
-	set_objects(background, image)
-	
-	var texture_size = image.texture.get_size()
-	image.pivot_offset = texture_size / 2
-	
+# Loading control
+var scene_path: String = ""
+var loading_complete := false
+
+func _ready() -> void:
+	bg_rect = $background
+	gear_image = $image
+
+	setup_ui()
+
+	# Center the image rotation around its midpoint
+	if gear_image.texture:
+		gear_image.pivot_offset = gear_image.texture.get_size() / 2
+
+	# Begin async loading
 	scene_path = get_path_to_level()
 	ResourceLoader.load_threaded_request(scene_path)
 
-func _process(delta):
-	var progress = []
+func _process(delta: float) -> void:
+	var progress := []
 	var status = ResourceLoader.load_threaded_get_status(scene_path, progress)
 
-	# Checks what is actually being loaded for the level	
-	#for dependency in ResourceLoader.get_dependencies(scene_path):
-		#print(dependency.get_slice("::", 0)) # Prints the UID.
-		#print(dependency.get_slice("::", 2)) # Prints the path.
-		
-	if status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-		$image.rotation_degrees += 360 * delta
-	elif status == ResourceLoader.THREAD_LOAD_LOADED:
-		get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
-		
-func set_objects(background, image):
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			gear_image.rotation_degrees += 360 * delta
+		ResourceLoader.THREAD_LOAD_LOADED:
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
+
+func setup_ui() -> void:
 	var screen_size = get_viewport_rect().size
-	var width = screen_size.x
-	var height = screen_size.y
+	var w = screen_size.x
+	var h = screen_size.y
 
-	# Background (ColorRect)
-	background.set_size(Vector2(width * 0.4, height * 0.8))
-	background.set_position(Vector2(width * 0.3, height * 0.1))
-	background.color = Color(77 / 255.0, 77 / 255.0, 77 / 255.0)
+	# Setup background
+	bg_rect.set_size(screen_size)
+	bg_rect.set_position(Vector2.ZERO)
+	bg_rect.color = Color(173 / 255.0, 216 / 255.0, 230 / 255.0)
 
-	# Background size & position
-	var bg_size = background.size
+	# Load gear texture
+	gear_image.texture = load("res://Assets/Interface/loading_screen/gear.png")
 
-	# Image (TextureRect) - Remove label & center image
-	image.texture = load("res://Assets/Interface/loading_screen/gear.png")
-	var image_scale_factor = 0.4
-	var max_image_height = bg_size.y * 0.3
-
-	if image.texture:
-		var texture_size = image.texture.get_size()
+	if gear_image.texture:
+		var texture_size = gear_image.texture.get_size()
 		var aspect_ratio = texture_size.x / float(texture_size.y)
 
-		var new_width = bg_size.x * image_scale_factor
-		var new_height = new_width / aspect_ratio
+		var max_height = h * 0.3
+		var desired_width = w * 0.4
+		var desired_height = desired_width / aspect_ratio
 
-		if new_height > max_image_height:
-			new_height = max_image_height
-			new_width = new_height * aspect_ratio
+		if desired_height > max_height:
+			desired_height = max_height
+			desired_width = desired_height * aspect_ratio
 
-		image.set_size(Vector2(new_width, new_height))
-		image.pivot_offset = image.size / 2  # Center rotation pivot
+		gear_image.set_size(Vector2(desired_width, desired_height))
+		gear_image.pivot_offset = gear_image.size / 2
 
-	# Center image in the middle of the screen
-	image.set_position(Vector2(
-		(width - image.size.x) / 2,
-		(height - image.size.y) / 2
-	))
-			
+		# Center the image
+		gear_image.set_position(Vector2(
+			(w - gear_image.size.x) / 2,
+			(h - gear_image.size.y) / 2
+		))
+
 func get_path_to_level() -> String:
 	match Global.level_to_play:
 		Global.levels.TUTORIAL:
@@ -75,4 +74,5 @@ func get_path_to_level() -> String:
 			return "res://Scenes/Levels/level_1.tscn"
 		Global.levels.LEVEL2:
 			return "res://Scenes/Levels/level_2.tscn"
-	return "res://Scenes/Interface/MainMenu.tscn"
+		_:
+			return "res://Scenes/Interface/MainMenu.tscn"
